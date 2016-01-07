ac14e8a1d406240d3618f1e958c13d293d99543d
Alex Wu <itswu.alex@gmail.com>
2016-01-06 19:45:00 -0800

    Revert "Soldier update"
    This reverts commit 3154d5fb297fa542f0ceb019f6f7a2d49455df95.

--- battlecode-scaffold/src/examplefuncsplayer/SoldierBrain.java
+++ battlecode-scaffold/src/examplefuncsplayer/SoldierBrain.java
@@ -1,6 +1,5 @@
 package examplefuncsplayer;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import battlecode.common.*;
@@ -8,9 +7,8 @@ import battlecode.common.*;
 public class SoldierBrain implements Brain {
 
 	static RobotController rc;
-    static ArrayList<MapLocation> past = new ArrayList<MapLocation>();
-    static int sameLoc = 0;
-    
+	static Direction last;
+
 	@Override
 	public void run(RobotController rcI) {
 		rc = rcI;
@@ -49,7 +47,6 @@ public class SoldierBrain implements Brain {
 		int myAttackRange = 0;
 		Team myTeam = rc.getTeam();
 		Team enemyTeam = myTeam.opponent();
-		MapLocation init = rc.getLocation();
 		boolean shouldAttack = false;
 		// If this robot type can attack, check for enemies within range and
 		// attack one
@@ -77,24 +74,12 @@ public class SoldierBrain implements Brain {
 				double L = rc.senseRubble(rc.getLocation().add(directions[0]));
 				for (int i = 0; i < 8; i++) {
 					Direction d = directions[i];
-					if (rc.canMove(d)&&rc.senseRubble(rc.getLocation().add(d)) < GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-						if (!past.contains(rc.getLocation().add(d))) {
+					if (rc.senseRubble(rc.getLocation().add(d)) < GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+						if ((i < 4 && last.equals(directions[i + 4]) == false) || (!last.equals(directions[i - 4]) == false)) {
 							move = true;
 							rc.move(d);
-							past.add(rc.getLocation());
-							if(past.size()>50)
-							{
-								past.remove(0);
-							}
+							last = d;
 							break;
-						}
-						else{
-							sameLoc++;
-							if(sameLoc>15)
-							{
-								for(int j=0;j<past.size()/5;j++)
-									past.remove(past.size()-1);
-							}
 						}
 					} else {
 						if (rc.senseRubble(rc.getLocation().add(d)) < L) {
