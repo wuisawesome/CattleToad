fde7de4dfe733ec2df410c4474d8a97a6735a71c
rkeyal <rahulkeyal@gmail.com>
2016-01-06 18:53:58 -0800

    Fixed up soldier class slightly
    Made more efficient through separate method, no random selection of
    stuff, moves randomly (but not opposite of last direction) and
    prioritizes movement over clearing rubble

--- battlecode-scaffold/src/examplefuncsplayer/SoldierBrain.java
+++ battlecode-scaffold/src/examplefuncsplayer/SoldierBrain.java
@@ -2,93 +2,96 @@ package examplefuncsplayer;
 
 import java.util.Random;
 
-import battlecode.common.Clock;
-import battlecode.common.Direction;
-import battlecode.common.GameConstants;
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-import battlecode.common.RobotType;
-import battlecode.common.Team;
+import battlecode.common.*;
 
-public class SoldierBrain implements Brain{
+public class SoldierBrain implements Brain {
+
+	static RobotController rc;
+	static Direction last;
 
 	@Override
-	public void run(RobotController rc) {
-		Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
-                Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
-        RobotType[] robotTypes = {RobotType.SCOUT, RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
-                RobotType.GUARD, RobotType.GUARD, RobotType.VIPER, RobotType.TURRET};
-        Random rand = new Random(rc.getID());
-        int myAttackRange = 0;
-        Team myTeam = rc.getTeam();
-        Team enemyTeam = myTeam.opponent();
-        
+	public void run(RobotController rcI) {
+		rc = rcI;
+		int myAttackRange = 0;
 		try {
-            // Any code here gets executed exactly once at the beginning of the game.
-            myAttackRange = rc.getType().attackRadiusSquared;
-        } catch (Exception e) {
-            // Throwing an uncaught exception makes the robot die, so we need to catch exceptions.
-            // Caught exceptions will result in a bytecode penalty.
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
+			// Any code here gets executed exactly once at the beginning of the
+			// game.
+			myAttackRange = rc.getType().attackRadiusSquared;
+		} catch (Exception e) {
+			// Throwing an uncaught exception makes the robot die, so we need to
+			// catch exceptions.
+			// Caught exceptions will result in a bytecode penalty.
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 
-        while (true) {
-            // This is a loop to prevent the run() method from returning. Because of the Clock.yield()
-            // at the end of it, the loop will iterate once per game round.
-            try {
-                int fate = rand.nextInt(1000);
+		while (true) {
+			// This is a loop to prevent the run() method from returning.
+			// Because of the Clock.yield()
+			// at the end of it, the loop will iterate once per game round.
+			try {
+				runTurn();
+				Clock.yield();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
 
-                if (fate % 5 == 3) {
-                    // Send a normal signal
-                    rc.broadcastSignal(80);
-                }
-
-                boolean shouldAttack = false;
-
-                // If this robot type can attack, check for enemies within range and attack one
-                if (myAttackRange > 0) {
-                    RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, enemyTeam);
-                    RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
-                    if (enemiesWithinRange.length > 0) {
-                        shouldAttack = true;
-                        // Check if weapon is ready
-                        if (rc.isWeaponReady()) {
-                            rc.attackLocation(enemiesWithinRange[rand.nextInt(enemiesWithinRange.length)].location);
-                        }
-                    } else if (zombiesWithinRange.length > 0) {
-                        shouldAttack = true;
-                        // Check if weapon is ready
-                        if (rc.isWeaponReady()) {
-                            rc.attackLocation(zombiesWithinRange[rand.nextInt(zombiesWithinRange.length)].location);
-                        }
-                    }
-                }
-
-                if (!shouldAttack) {
-                    if (rc.isCoreReady()) {
-                        if (fate < 600) {
-                            // Choose a random direction to try to move in
-                            Direction dirToMove = directions[fate % 8];
-                            // Check the rubble in that direction
-                            if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
-                                // Too much rubble, so I should clear it
-                                rc.clearRubble(dirToMove);
-                                // Check if I can move in this direction
-                            } else if (rc.canMove(dirToMove)) {
-                                // Move
-                                rc.move(dirToMove);
-                            }
-                        }
-                    }
-                }
-
-                Clock.yield();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-        }
+	public static void runTurn() throws GameActionException {
+		Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
+				Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST };
+		RobotType[] robotTypes = { RobotType.SCOUT, RobotType.SOLDIER, RobotType.SOLDIER, RobotType.SOLDIER,
+				RobotType.GUARD, RobotType.GUARD, RobotType.VIPER, RobotType.TURRET };
+		int myAttackRange = 0;
+		Team myTeam = rc.getTeam();
+		Team enemyTeam = myTeam.opponent();
+		boolean shouldAttack = false;
+		// If this robot type can attack, check for enemies within range and
+		// attack one
+		if (rc.getType().canAttack() && myAttackRange > 0) {
+			RobotInfo[] enemiesWithinRange = rc.senseNearbyRobots(myAttackRange, enemyTeam);
+			RobotInfo[] zombiesWithinRange = rc.senseNearbyRobots(myAttackRange, Team.ZOMBIE);
+			if (enemiesWithinRange.length > 0) {
+				shouldAttack = true;
+				// Check if weapon is ready
+				if (rc.isWeaponReady()) {
+					rc.attackLocation(enemiesWithinRange[0].location);
+				}
+			} else if (zombiesWithinRange.length > 0) {
+				shouldAttack = true;
+				// Check if weapon is ready
+				if (rc.isWeaponReady()) {
+					rc.attackLocation(zombiesWithinRange[0].location);
+				}
+			}
+		}
+		if (!shouldAttack) {
+			if (rc.isCoreReady()) {
+				Boolean move = false;
+				Direction lowest = directions[0];
+				double L = rc.senseRubble(rc.getLocation().add(directions[0]));
+				for (int i = 0; i < 8; i++) {
+					Direction d = directions[i];
+					if (rc.senseRubble(rc.getLocation().add(d)) < GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+						if ((i < 4 && last.equals(directions[i + 4]) == false) || (!last.equals(directions[i - 4]) == false)) {
+							move = true;
+							rc.move(d);
+							last = d;
+							break;
+						}
+					} else {
+						if (rc.senseRubble(rc.getLocation().add(d)) < L) {
+							lowest = d;
+						}
+					}
+				}
+				if (!move) {
+					rc.clearRubble(lowest);
+				}
+			}
+		}
 	}
 
 }
